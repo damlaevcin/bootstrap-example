ee5494a3dadee49ac821a5ec653313e50897ade5
damlaevcin <dmlaevcinn@gmail.com>
2024-07-14 17:01:19 +0300

    jfjf

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// $(document).on("click", ".launch-button", function () {
+//     Swal.fire({
+//         title: "The Internet?",
+//         text: "That thing is still around?",
+//         icon: "question"
+//     });
+// })
+
+// $(document).on("keydown", function (e) {
+//     console.log(e.key);
+
+// })
+
+
+// $(document).on("click", ".launch-button", function () {
+//     var sayi1 = parseInt($(".sayi-1").val());
+//     var sayi2 = $(".sayi-2").val();
+//     console.log(sayi1 + parseInt(sayi2));
+
+// })
+
+var x = [
+    { a: 2 },
+    "elma",
+    "portakal",
+    "armut",
+    "erik",
+    "fişne"
+];
+
+x.push("kiwi")
+// console.log(x)
+
+// x.forEach(function (value, key){
+//     console.log(key)
+// })
+
+var damla = {
+    yas: 17,
+    boy: 160,
+    hobiler: ["yatmak", "uyumak", "yemek yemek", "oturmak"]
+}
+
+
+for (var key in damla){
+    console.log(damla[key])
+}
+
+
+
+// damla.hobiler.forEach(function(value, key){
+//     console.log(key, value)
+// })
