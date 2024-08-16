4acdedcc936797ae1ef110b27f3084c6f1d8e000
damlaevcin <dmlaevcinn@gmail.com>
2024-08-16 21:57:48 +0300

    messsage

--- js/main.js
+++ js/main.js
@@ -42,11 +42,42 @@ var damla = {
 }
 
 
-for (var key in damla){
+
+for (var key in damla) {
     console.log(damla[key])
 }
 
 
+function cikart(x, y) {
+    return x - y;
+}
+
+
+console.log(cikart(10, 5));
+
+function topla(status) {
+    var sayi1 = $(".sayi-1").val();
+    var sayi2 = $(".sayi-2").val();
+    var toplam = parseInt(sayi1) + parseInt(sayi2);
+    Swal.fire({
+        position: "top-end",
+        icon: status,
+        title: `Toplam: ${toplam}`,
+        showConfirmButton: false,
+        timer: 1500
+    });
+}
+
+$(document).on("click", ".launch-button", function () {
+    topla("error")
+
+})
+$(document).on("keydown", function (e) {
+    console.log(e)
+    if (e.key === "Enter") {
+        topla("success");
+    }
+})
 
 // damla.hobiler.forEach(function(value, key){
 //     console.log(key, value)
